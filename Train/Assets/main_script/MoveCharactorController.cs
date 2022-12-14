dd74b4ad55a461048f9cff95996a4270b588fd0c
eboshidori-pen <64635773+eboshidori-pen@users.noreply.github.com>
2022-12-14 18:32:29 +0900

    ライトを付け足した。画像を付け足した

--- Train/Assets/main_script/MoveCharactorController.cs
+++ Train/Assets/main_script/MoveCharactorController.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class MoveCharactorController : MonoBehaviour
 {
     private float speed = 0.005f;
+    private SpriteRenderer renderer;
 
     void Start()
     {
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         Vector2 position = transform.position;
 
-        if (Input.GetKey("left"))
+        if (Input.GetKey("a"))
         {
             position.x -= speed;
+            renderer.flipX = true;
+            
         }
-        else if (Input.GetKey("right"))
+        else if (Input.GetKey("d"))
         {
             position.x += speed;
+            renderer.flipX = false;
         }
-        else if (Input.GetKey("up"))
+        else if (Input.GetKey("w"))
         {
             position.y += speed;
         }
-        else if (Input.GetKey("down"))
+        else if (Input.GetKey("s"))
         {
             position.y -= speed;
         }
