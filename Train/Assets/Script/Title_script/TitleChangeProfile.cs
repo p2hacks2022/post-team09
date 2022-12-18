0965fe2e440d0a3c863877699bf9962586eb8369
eboshidori-pen <64635773+eboshidori-pen@users.noreply.github.com>
2022-12-18 11:30:48 +0900

    最終調整

---
+++ Train/Assets/Script/Title_script/TitleChangeProfile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleChangeProfile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private bool firstPush = false;
+    [SerializeField] GameObject titleUI;
+    [SerializeField] GameObject gameProfileUI;
+    private void Start()
+    {
+        gameProfileUI.SetActive(false);
+    }
+
+    public void PressStart()
+    {
+        
+        //if (!firstPush)
+        //{
+            titleUI.SetActive(false); 
+            gameProfileUI.SetActive(true);
+            
+
+
+           // firstPush = true;
+        //}
+    }
+
+}
